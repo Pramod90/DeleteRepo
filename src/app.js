d3ecf56766b16021690fd6e65f7484ca4d92db03
unknown <pramod.neerchal@gmail.com>
2020-05-15 14:52:59 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+
+const app = express();
+//const port = 3000;
+const port = process.env.PORT;
+
+app.get('',(req,res)=>{
+    res.send({
+        error : 'No url found for requested api',
+    });
+});
+
+app.get('/help',(req,res)=>{
+    res.send({
+        helperName : 'Pramod',
+        helperAge : '33',
+    });
+});
+
+app.listen(port,()=>{
+    console.log('Listening on port 3000');
+})
